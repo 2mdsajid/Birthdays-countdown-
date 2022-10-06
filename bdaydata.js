3821ba7484e9467864167fdfd6ba38dd021b9650
2mdsajid <69730158+2mdsajid@users.noreply.github.com>
2022-10-06 11:19:57 +0545

    Add files via upload

---
+++ bdaydata.js
@@ -0,0 +1,208 @@
+data = [{
+   "name":" Alpu",
+   "bday":" aug 4"
+},
+{
+   "name":" Aaditya ",
+   "bday":" sep 17"
+},
+{
+   "name":" Aanchal ",
+   "bday":" june 20"
+},
+{
+   "name":" Akanksha ",
+   "bday":" dec 1"
+},
+{
+   "name":" Apina ",
+   "bday":" July 19"
+},
+{
+   "name":" Anushka ",
+   "bday":" may 11"
+},
+{
+   "name":" Anuragh ",
+   "bday":" sep 9"
+},
+{
+   "name":" Abhilekh ",
+   "bday":"Dec 19"
+},
+{
+   "name":" Akhilesh ",
+   "bday":" aug 30"
+},
+{
+   "name":" Aisha ",
+   "bday":" nov 22"
+},
+{
+   "name":" Anisha ",
+   "bday":" Dec 16"
+},
+{
+   "name":" Amar ",
+   "bday":" July 28"
+},
+{
+   "name":" Aryan ",
+   "bday":" aug 28"
+},
+{
+   "name":" Aayush ",
+   "bday":" aug 18"
+},
+{
+   "name":" Binayak ",
+   "bday":" aug 1"
+},
+{
+   "name":" Bishal ",
+   "bday":" apr 16"
+},
+{
+   "name":" Biju ",
+   "bday":" apr 24"
+},
+{
+   "name":" Barun ",
+   "bday":" jan 19"
+},
+{
+   "name":" Gaurab ",
+   "bday":" jul 4"
+},
+{
+   "name":" Grishma ",
+   "bday":" sep 4"
+},
+{
+   "name":" Hritika ",
+   "bday":" July 30"
+},
+{
+   "name":" Hritesh ",
+   "bday":" july 21"
+},
+{
+   "name":" Kunal ",
+   "bday":" may 21"
+},
+{
+   "name":" Laxman ",
+   "bday":" Nov 31"
+},
+{
+   "name":" Manas ",
+   "bday":" Aug 20"
+},
+{
+   "name":" Manisha ",
+   "bday":" nov 25"
+},
+{
+   "name":" Mayank ",
+   "bday":" sep 25"
+},
+{
+   "name":" Nishant ",
+   "bday":" jan 25"
+},
+{
+   "name":" Neha ",
+   "bday":" may 2"
+},
+{
+   "name":" Puru ",
+   "bday":" june 11"
+},
+{
+   "name":" Purak ",
+   "bday":" aug 23"
+},
+{
+   "name":" Pranita ",
+   "bday":" nov 28"
+},
+{
+   "name":" Prerna S ",
+   "bday":" oct 9"
+},
+{
+   "name":" Prabej ",
+   "bday":" jan 1"
+},
+{
+   "name":" Ranjan ",
+   "bday":" may 27"
+},
+{
+   "name":" Raazz ",
+   "bday":" jan 31"
+},
+{
+   "name":" Sardo ",
+   "bday":" jan 31"
+},
+{
+   "name":" Sachin ",
+   "bday":" feb 14"
+},
+{
+   "name":" Sajina ",
+   "bday":" june 29"
+},
+{
+   "name":" Sneha",
+   "bday":" june 24"
+},
+{
+   "name":" Shruti ",
+   "bday":" Aug 29"
+},
+{
+   "name":" Suprasanna ",
+   "bday":" may 11"
+},
+{
+   "name":" Sumiran ",
+   "bday":" aug 24"
+},
+{
+   "name":" Suraj ",
+   "bday":" nov 6"
+},
+{
+   "name":" Shijan ",
+   "bday":" may 6"
+},
+{
+   "name":" Seeja ",
+   "bday":" apr 9"
+},
+{
+   "name":" Smriti ",
+   "bday":" jan 16"
+},
+{
+   "name":" Subham ",
+   "bday":" jan 6"
+},
+{
+   "name":" Syrus ",
+   "bday":" aug 11"
+},
+{
+   "name":" Umesh ",
+   "bday":" aug 14"
+},
+{
+   "name":" Visika ",
+   "bday":" apr 21"
+},
+{
+   "name":" Veneesa ",
+   "bday":" jan 23"
+}]
